bdb629c8bcdc5ae31d52249eadfd10ed685beccc
jongithub59 <jdelrealj01@gmail.com>
2024-07-29 16:16:11 -0500

    added base HTML page layout + screenController.js

---
+++ src/modules/ScreenController.js
@@ -0,0 +1,26 @@
+const Gameboard = require("../Gameboard");
+const Ship = require("../Ship");
+const Player = require("../Player");
+
+class ScreenController {
+    setupGame() {
+
+    }
+    createPlayerBaord(player) {
+
+    }
+
+    createComputerBaord() {
+
+    }
+
+    playRound() {
+
+    }
+
+    endGame(winner) {
+
+    }
+}
+
+module.exports = ScreenController;
